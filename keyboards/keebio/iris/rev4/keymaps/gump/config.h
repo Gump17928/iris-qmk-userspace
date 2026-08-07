e1cf584f9cf3557b1d2dfd857649651154247c8e
Forrest Nichols <forrestdn123@gmail.com>
2026-08-07 12:04:40 -0600

    Raise DYNAMIC_KEYMAP_LAYER_COUNT to 5 for VIA

--- keyboards/keebio/iris/rev4/keymaps/gump/config.h
+++ keyboards/keebio/iris/rev4/keymaps/gump/config.h
@@ -14,3 +14,7 @@
 
 // Turn the underglow off when the host goes to sleep.
 #define RGBLIGHT_SLEEP
+
+// VIA reserves EEPROM for a fixed number of remappable layers (default 4).
+// We ship 5 (_BASE, _LOWER, _RAISE, _ADJUST, _GAMING), so raise the cap.
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
