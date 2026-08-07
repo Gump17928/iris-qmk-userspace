6d1701d3ceef79c9a92aa3c7a80801454a052730
Forrest Nichols <forrestdn123@gmail.com>
2026-08-07 12:00:30 -0600

    Enable VIA for runtime keymap edits via usevia.app

--- keyboards/keebio/iris/rev4/keymaps/gump/rules.mk
+++ keyboards/keebio/iris/rev4/keymaps/gump/rules.mk
@@ -3,3 +3,6 @@
 
 # WS2812 underglow (6 LEDs per side, 12 total). Configured in config.h.
 RGBLIGHT_ENABLE = yes
+
+# VIA runtime remapping + macro editor (usevia.app). Persists to EEPROM.
+VIA_ENABLE = yes
